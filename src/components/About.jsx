6e7f1d176c89347042cde358cb14bc3d627c1fbe
Bildad Ochieng Masaga <bijasoto@gmail.com>
2026-01-15 23:30:44 +0300

    updated and rendered the props

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Links from "./Links";
-function About() {
+
+function About({ bio, links }) {
     return (
         <div id="about">
-            About!
-            <Links />
+            <h2>About Me</h2>
+            <p>{bio}</p>
+            <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+            <Links github={links.github} linkedin={links.linkedin} />
         </div>
     );
 }
+
 export default About;
--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-function Links() {
+
+function Links({ github, linkedin }) {
     return (
-        <div id="links">
-            Links!
+        <div>
+            <h3>Links</h3>
+            <a href={github}>{github}</a>
+            <a href={linkedin}>{linkedin}</a>
         </div>
     );
 }
+
 export default Links;
