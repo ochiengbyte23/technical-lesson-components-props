021e0d508d33e08f601cab6162654cf50978129e
Bildad Ochieng Masaga <bijasoto@gmail.com>
2026-01-15 23:20:48 +0300

    updated the Navigation dynamics

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-
 function NavBar() {
     return (
-        <div id="navbar">
-            Navbar!
-        </div>
+        <nav>
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+        </nav>
     );
 }
 export default NavBar;
