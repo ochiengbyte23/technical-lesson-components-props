870648eec55f1556c9eff731176de8567ecce718
Bildad Ochieng Masaga <bijasoto@gmail.com>
2026-01-15 23:08:36 +0300

    added user data to the App.jsx file and created child props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,15 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
+import user from "../data/user";
+console.log(user);
+
 function App() {
   return (
     <div>
-      {/* add the <Navbar> component here */}
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <NavBar />
+      <Home />
+      <About />
     </div>
   );
 }
